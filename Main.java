0a9a9c35998fdee2246a73bda0088dc71012e5e1
Girlc7442 <girlc7442@wrdsb.ca>
2020-09-14 18:03:50 +0000

    finished the slope calculator with comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Calculates the slope of a line
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //asks the user for the first x coordinate
+    System.out.println("Please enter the first x coordinate");
+    //initializes the first x coordinate
+    int xOne = input.nextInt();
+
+    //asks the user for the first y coordinate
+    System.out.println("Please enter the first y coordinate");
+    //initializes the first y coordinate
+    int yOne = input.nextInt();
+
+    //asks the user for the second x coordinate
+    System.out.println("Please enter the second x coordinate");
+    //initializes the second x coordinate
+    int xTwo = input.nextInt();
+
+    //asks the user for the second y coordinate
+    System.out.println("Please enter the second y coordinate");
+    //initializes the second y coordinate
+    int yTwo = input.nextInt();
+
+    //Calculates the slope of the line
+    final double formula = (yTwo - yOne) / (xTwo - xOne);
+
+    //Prints the slope to the screen
+    System.out.println(formula);
     
   }
 }
